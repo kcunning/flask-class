536259c5e108929c8dc005b8422ed87f51379f8c
Katie Cunningham <katie.fulton@gmail.com>
2013-09-21 13:58:53 -0400

    Added a game to show for loops

--- flaskclass/app/views.py
+++ flaskclass/app/views.py
@@ -12,3 +12,12 @@ def lucky_static():
 def lucky_max(max):
 	lucky_num = randint(1, int(max))
 	return render_template('simple.html', lucky_num=lucky_num)
+
+@app.route('/game/')
+def game():
+	game_nums = []
+	while len(game_nums) < 5:
+		n = randint(1, 10)
+		if not n in game_nums:
+			game_nums.append(n)
+	return render_template('game.html', game_nums = game_nums)
