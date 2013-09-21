c272bbc8f5642e052f240f1eca0d031685bf72ad
Katie Cunningham <katie.fulton@gmail.com>
2013-09-21 10:32:03 -0400

    Using a basic template

--- flaskclass/app/views.py
+++ flaskclass/app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def lucky_static():
 	return "Your lucky number is 7"
+
+@app.route('/tpl/')
+def lucky_tpl():
+	return render_template('simple.html')
