ffe21224a70794426e148654f87f283fd8b81252
Katie Cunningham <katie.fulton@gmail.com>
2013-09-28 08:50:59 -0400

    We now get numbers from the user and go to a different page if we've submitted the form

--- flaskclass/app/views.py
+++ flaskclass/app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 from forms import GetLucky
 
@@ -15,16 +15,27 @@ def lucky_max(max):
 	lucky_num = randint(1, int(max))
 	return render_template('simple.html', lucky_num=lucky_num)
 
-@app.route('/game/')
-def game():
+def get_game_nums(num=5, max=10):
 	game_nums = []
-	while len(game_nums) < 5:
+	while len(game_nums) < num:
 		n = randint(1, 10)
 		if not n in game_nums:
 			game_nums.append(n)
+	return game_nums
+
+
+@app.route('/game/')
+def game():
+	game_nums = get_game_nums()
 	return render_template('game.html', game_nums = game_nums)
 
 @app.route('/nums/', methods=['GET', 'POST'])
 def get_nums():
 	form = GetLucky()
-	return render_template('get_lucky.html', form=form)
+	if request.method == 'POST':
+		numbers = form.numbers.data
+		game_nums = get_game_nums()	
+		return render_template('game.html', game_nums=game_nums,
+					player_nums=numbers)
+	if request.method == 'GET':	
+		return render_template('get_lucky.html', form=form)
