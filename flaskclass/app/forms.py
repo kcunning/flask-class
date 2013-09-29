ee49b28499f09b9abe53d24984dc553e312a5c2e
Katie Cunningham <katie.fulton@gmail.com>
2013-09-29 09:24:53 -0400

    Added errors to go with our form validation

--- flaskclass/app/forms.py
+++ flaskclass/app/forms.py
@@ -8,10 +8,17 @@ class GetLucky(Form):
 	def validate(self):
 		nums = self.numbers.data.split()
 		valid = True
+		errors = []
+
 		for num in nums:
 			if not num.isdigit():
 				valid = False
+				errors.append('One of the items is not a number')
+
 		if len(nums) != 3:
 			valid = False
+			errors.append('You may submit exactly three numbers.')
+
+		self.numbers.errors = tuple(errors)
 
 		return valid
